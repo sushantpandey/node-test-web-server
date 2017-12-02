45e9b7ac8e44c5374a9e557191089d9bce247772
Sushant Pandey <sushant.pandey@gmail.com>
2017-12-02 15:12:51 +0530

    adding new page projects

--- server.js
+++ server.js
@@ -57,6 +57,12 @@ app.get('/about', (req, res) => {
   })
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'projects Page'
+  })
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to process',
